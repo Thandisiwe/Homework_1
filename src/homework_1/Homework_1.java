c3a9d20f98e0fda97c0e9126197179747da2c6a6
Mthokwa Thandisiwe <Mthokwa Thandisiwe@LAPTOP-KQMFU2TH>
2024-08-08 14:26:12 +0200

    Homework_1

---
+++ src/homework_1/Homework_1.java
@@ -0,0 +1,17 @@
+
+package homework_1;
+
+/**
+ *Student Name: Thandisiwe Mthokwa
+ * Student Number: ST10440247
+ * Assignment Name: Homework 1
+ * Submission due date: 28 July 2024
+ */
+public class Homework_1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!!");
+    }
+    
+}
